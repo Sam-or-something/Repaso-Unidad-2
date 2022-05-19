e61fe998b394e19628243e3cc746f20519e1c94d
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-05-19 09:06:54 -0300

    Exercise 4 Button

---
+++ Assets/Scripts/EjBoton4.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EjBoton4 : MonoBehaviour
+{
+    public GameObject inputObject;
+
+    public void activarDesactivar()
+    {
+        if (!inputObject.activeInHierarchy)
+        {
+            inputObject.SetActive(true);
+        }
+        else
+        {
+            inputObject.SetActive(false);
+        }
+    }
+}
