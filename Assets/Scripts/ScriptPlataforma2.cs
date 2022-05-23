dffdbdcfd294ceb65ad55f9dc8958fd67f783077
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-05-23 15:53:28 -0300

    MovingPlatform

---
+++ Assets/Scripts/ScriptPlataforma2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPlataforma2 : MonoBehaviour
+{
+    public float vel, limitRight, limitLeft;
+    public bool RoL;
+    public GameObject prefab1, prefab2;
+
+    void Update()
+    {
+        if (RoL)
+        {
+            transform.position += new Vector3(vel, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(vel, 0, 0);
+        }
+
+        if(transform.position.x > limitRight)
+        {
+            RoL = false;
+            Instantiate(prefab1);
+        }
+        else if (transform.position.x < limitLeft)
+        {
+            RoL = true;
+            Instantiate(prefab2);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPlatform : MonoBehaviour
+{
+    public float vel, limitPos, limitNeg;
+    public bool RoL;
+
+    void Update()
+    {
+        if (RoL && transform.position.x < limitPos)
+        {
+            transform.position += new Vector3(vel, 0, 0);
+        }
+        else
+        {
+            RoL = false;
+        }
+
+        if (!RoL && transform.position.x > limitNeg)
+        {
+            transform.position -= new Vector3(vel, 0, 0);
+        }  
+        else
+        {
+            RoL = true;
+        }
+    }
+}
